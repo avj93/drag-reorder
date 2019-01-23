9f09be5a660637124944641d80ddf9d9ac518ce7
hrishikeshqb <hrishikesh@qburst.com>
2019-01-23 07:38:29 +0530

    Updates

---
+++ precache-manifest.01b3b970dbc42e7d3ad49cafbf54c716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "099cdbdef0c0e6e13405",
+    "url": "/drag-reorder/static/js/runtime~main.099cdbde.js"
+  },
+  {
+    "revision": "2f21c537e3dbe265d9d7",
+    "url": "/drag-reorder/static/js/main.2f21c537.chunk.js"
+  },
+  {
+    "revision": "771df12728dd4f4c5f53",
+    "url": "/drag-reorder/static/js/1.771df127.chunk.js"
+  },
+  {
+    "revision": "2f21c537e3dbe265d9d7",
+    "url": "/drag-reorder/static/css/main.a253c365.chunk.css"
+  },
+  {
+    "revision": "8b5673d7048625c591c39c3dfac7a406",
+    "url": "/drag-reorder/index.html"
+  }
+];
